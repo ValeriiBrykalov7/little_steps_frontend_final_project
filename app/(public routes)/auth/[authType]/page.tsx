20650971bbbfa6095cca5a81b8d67bd57005a48d
Tatiana <tany663@gmail.com>
2026-04-26 21:52:50 +0300

    toggleLoginRegister

---
+++ app/(public routes)/auth/[authType]/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+import LoginForm from '@/components/LoginForm/LoginForm';
+import styles from './page.module.css';
+import { useParams } from 'next/navigation';
+
+
+export default function ToggleLoginRegister() {
+  const params = useParams();
+  const authType = params.authType as string;
+  console.log('Auth Type:', authType);
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.formContainer}>
+        {authType === 'login' && <LoginForm />}
+        {/* {authType === 'register' && <RegistrationForm/>} */}
+      </div>
+    </main>
+  );
+}
