1759c1c9df82acce886138db22b2220468e5e967
MaxfeedLeyn <maxnebelskiy@gmail.com>
2026-04-28 01:58:15 +0300

    Add DiaryList, + icon-create icon

---
+++ components/DiaryList/DiaryList.tsx
@@ -0,0 +1,31 @@
+import css from './DiaryList.module.css';
+import type { DiaryEntry } from '@/types/diary';
+import Link from 'next/link';
+interface DiaryListProps {
+  diaries: DiaryEntry[];
+}
+
+const DiaryList = ({ diaries }: DiaryListProps) => {
+  return (
+    <div className={css.diaryListContainer}>
+      <div className={css.diaryNavigation}>
+        <h2 className={css.diaryNavigationTitle}>Ваші записи</h2>
+        <Link href='/diary/action/create' className={css.diaryNavigationButton}>
+          <span className={css.diaryNavigationButtonText}>Новий запис</span>
+          <svg width="21" height="21" viewBox="0 0 21 21" className={css.diaryNavigationButtonIcon}>
+            <use href='/sprite.svg#icon-create'></use>
+          </svg>
+        </Link>
+      </div>
+      <ul className={css.diaryList}>
+        {diaries.map((diary) => (
+          // інший компонент
+          <li className={css.diaryEntry} key={diary._id}>
+            <h3 className={css.diaryEntryTitle}>{diary.title}</h3>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default DiaryList;
