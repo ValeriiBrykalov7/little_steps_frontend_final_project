8b6bfe9295ff96a52b11607c4c22ae48560cc36a
andriy <and123bar@gmail.com>
2026-04-27 13:46:07 +0300

    week-selector

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { WeekSelector } from "@/components/WeekSelector/WeekSelector";
 export default function Home() {
   return (
     <>
-      <WeekSelector initialWeek={19} />
+      
     </>
   );
 }
