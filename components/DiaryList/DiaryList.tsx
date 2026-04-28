e2569142a645485a8ea5c2e1a51dcdd03bcdc046
MaxfeedLeyn <maxnebelskiy@gmail.com>
2026-04-28 18:01:36 +0300

    Add comments, what should be done

--- components/DiaryList/DiaryList.tsx
+++ components/DiaryList/DiaryList.tsx
@@ -10,6 +10,7 @@ const DiaryList = ({ diaries }: DiaryListProps) => {
     <div className={css.diaryListContainer}>
       <div className={css.diaryNavigation}>
         <h2 className={css.diaryNavigationTitle}>Ваші записи</h2>
+        //TODO - добавити справжній шлях до створення запису
         <Link href='/diary/action/create' className={css.diaryNavigationButton}>
           <span className={css.diaryNavigationButtonText}>Новий запис</span>
           <svg width="21" height="21" viewBox="0 0 21 21" className={css.diaryNavigationButtonIcon}>
@@ -19,7 +20,7 @@ const DiaryList = ({ diaries }: DiaryListProps) => {
       </div>
       <ul className={css.diaryList}>
         {diaries.map((diary) => (
-          // інший компонент
+          //TODO - добавити справжній компонент сюди
           <li className={css.diaryEntry} key={diary._id}>
             <h3 className={css.diaryEntryTitle}>{diary.title}</h3>
           </li>
