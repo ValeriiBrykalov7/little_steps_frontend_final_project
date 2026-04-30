e07f1193f6659d35734a00f5f6a5922b93f596ea
Anna Anishchenko <annuwkaanishchenko@gmail.com>
2026-04-30 20:52:29 +0300

    README

--- components/GreetingBlock/GreetingBlock.tsx
+++ components/GreetingBlock/GreetingBlock.tsx
@@ -4,13 +4,23 @@ import css from './GeetingBlock.module.css';
 const GreetingBlock = () => {
   const { isAuthenticated, user } = useAuthStore();
 
+  const getGreeting = () => {
+    const hour = new Date().getHours();
+    if (hour >= 6 && hour < 12) return 'Доброго ранку';
+    if (hour >= 12 && hour < 18) return 'Доброго дня';
+    if (hour >= 18 && hour < 24) return 'Доброго вечора';
+    return 'Доброї ночі';
+  };
+
   return (
     <div className={`container ${css.greeting_block}`}>
       {' '}
       {isAuthenticated ? (
-        <p className={css.greeting_text}>Доброго ранку, {user?.username}! </p>
+        <p className={css.greeting_text}>
+          {getGreeting()}, {user?.username}!{' '}
+        </p>
       ) : (
-        <p className={css.greeting_text}>Доброго ранку, гість!</p>
+        <p className={css.greeting_text}>{getGreeting()}, гість!</p>
       )}
     </div>
   );
