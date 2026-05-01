ce0faa435604f1d4a49bec97758e944402a052d3
Dmytrii Datskevych <datskevychdv@gmail.com>
2026-05-01 15:20:47 +0200

    chore: remove commented-out code

--- components/GreetingBlock/GreetingBlock.tsx
+++ components/GreetingBlock/GreetingBlock.tsx
@@ -1,24 +1,3 @@
-// import { useAuthStore } from '@/lib/store/authStore';
-// import css from './GeetingBlock.module.css';
-
-// const GreetingBlock = () => {
-//   const { isAuthenticated, user } = useAuthStore();
-
-//   return (
-//     <div className={`container ${css.greeting_block}`}>
-//       {' '}
-//       {isAuthenticated ? (
-//          <p className={css.greeting_text}>
-//         Доброго ранку, {user?.username ? (user.username.charAt(0).toUpperCase() + user.username.slice(1).toLowerCase()) : 'Гість'}!
-//       </p>
-//       ) : (
-//         <p className={css.greeting_text}>Доброго ранку, гість!</p>
-//       )}
-//     </div>
-//   );
-// };
-// export default GreetingBlock;
-
 import { useAuthStore } from '@/lib/store/authStore';
 import css from './GeetingBlock.module.css';
 
