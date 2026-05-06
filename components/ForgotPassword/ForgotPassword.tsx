855f7ae1c64ccdcda9cc9380c0d597af706d0570
Ostapenko Bogdan <bogdan.ostapenko.97@gmail.com>
2026-05-07 00:23:38 +0300

    add zahlushka

--- components/ForgotPassword/ForgotPassword.tsx
+++ components/ForgotPassword/ForgotPassword.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import { toast } from 'react-hot-toast';
@@ -7,15 +8,27 @@ import { requestResetEmail } from '@/lib/api/clientApi';
 import Link from 'next/link';
 
 export default function ForgotPassword() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
   const handleSubmit = async (values: { email: string }) => {
     try {
       await requestResetEmail(values);
-
-      toast.success('Якщо email вірний, лист вже летить до вас!');
+      toast.success('Лист відправлено!');
+      setIsSubmitted(true); // Показуємо заглушку
     } catch (error) {
       toast.error('Щось пішло не так на сервері');
     }
   };
+
+  if (isSubmitted) {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h2>Перевірте вашу пошту</h2>
+        <p>Ми надіслали посилання для відновлення пароля на ваш email.</p>
+        <Link href='/auth/login'>Повернутися до входу</Link>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -24,13 +37,17 @@ export default function ForgotPassword() {
         initialValues={{ email: '' }}
         onSubmit={handleSubmit}
         validationSchema={Yup.object({
-          email: Yup.string().email().required(),
+          email: Yup.string().email('Невірний формат').required('Обов’язково'),
         })}
       >
-        <Form>
-          <Field name='email' type='email' placeholder='Введіть ваш email' />
-          <button type='submit'>Надіслати лист</button>
-        </Form>
+        {({ isSubmitting }) => (
+          <Form>
+            <Field name='email' type='email' placeholder='Введіть ваш email' />
+            <button type='submit' disabled={isSubmitting}>
+              {isSubmitting ? 'Надсилаємо...' : 'Надіслати лист'}
+            </button>
+          </Form>
+        )}
       </Formik>
       <Link href='/auth/login'>Повернутися до входу</Link>
     </div>
